711e32cfff96cd1fc28105bef72911994d7edf83
alghafli <37610446+alghafli@users.noreply.github.com>
2018-03-21 11:21:09 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 2 - Beta',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
